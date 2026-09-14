b96613fa4d18404aa21dcd6757f0c2998132d074
Quiet Path build <quiet-path@users.noreply.github.com>
2026-09-14 13:33:21 +0300

    Fix overlapping and interrupted mobile swipes

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789345957|2231528';
+const CACHE_VERSION = '1789381781|2425060';
 /** @type {string} */
 const CACHE_PREFIX = 'Ёжик в тумане — -sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
@@ -17,7 +17,7 @@ const ENSURE_CROSSORIGIN_ISOLATION_HEADERS = true;
 const CACHED_FILES = ["index.html","index.js","index.offline.html","index.icon.png","index.apple-touch-icon.png","index.audio.worklet.js","index.audio.position.worklet.js"];
 // Files that we might not want the user to preload, and will only be cached on first load.
 /** @type {string[]} */
-const CACHEABLE_FILES = ["index.wasm","index-v6.3.pck"];
+const CACHEABLE_FILES = ["index.wasm","index-v6.3.1.pck"];
 const FULL_CACHE = CACHED_FILES.concat(CACHEABLE_FILES);
 
 self.addEventListener('install', (event) => {
